f8f353bbd14c51e75ef7f7dbb107a2d91b1e8cce
ImadIdaliouali <imadidaliouali@gmail.com>
2023-12-02 19:45:08 +0100

    Arrays & Object Literals

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,28 @@
 "use strict";
-console.log("Hello World!");
+// arrays
+let names = ["Imad", "Oualid", "Mehdi"];
+let ages = [19, 20];
+names.push("Yubie");
+ages.push(18);
+// type inference with arrays
+let fruits = ["apples", "pears", "bananas", "mangos"];
+fruits.push("peaches");
+const f = fruits[3];
+let things = [1, true, "hello"];
+const t = things[0];
+// object literals
+let user = {
+    firstName: "Someone",
+    age: 20,
+    id: 2,
+};
+user.firstName = "Imad";
+user.id = 1;
+// type inference with object literals
+let person = {
+    name: "Someone",
+    score: 35,
+};
+person.name = "Imad";
+person.score = 65;
+const score = person.score;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,38 @@
-console.log("Hello World!");
+// arrays
+let names: string[] = ["Imad", "Oualid", "Mehdi"];
+let ages: number[] = [19, 20];
+
+names.push("Yubie");
+ages.push(18);
+
+// type inference with arrays
+let fruits = ["apples", "pears", "bananas", "mangos"];
+
+fruits.push("peaches");
+
+const f = fruits[3];
+
+let things = [1, true, "hello"];
+
+const t = things[0];
+
+// object literals
+let user: { firstName: string; age: number; id: number } = {
+  firstName: "Someone",
+  age: 20,
+  id: 2,
+};
+
+user.firstName = "Imad";
+user.id = 1;
+
+// type inference with object literals
+let person = {
+  name: "Someone",
+  score: 35,
+};
+
+person.name = "Imad";
+person.score = 65;
+
+const score = person.score;
