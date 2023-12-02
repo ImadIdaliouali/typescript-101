43ffeb5c7b8e65e6a571df960ea6c0c99269706f
ImadIdaliouali <imadidaliouali@gmail.com>
2023-12-02 18:23:10 +0100

    Type Basics

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+// number & string & boolean
 var age = 30;
-console.log(age);
+var firstName = "Test";
+var isFictional;
+age = 20;
+firstName = "Imad";
+isFictional = false;
+var planet = "Earth";
+var moons = 1;
+var isLarge = false;
+planet = "Mars";
+moons = 145;
+isLarge = true;
+// null & undefined
+var something;
+var anthorThing;
+something = null;
+anthorThing = undefined;
--- index.ts
+++ index.ts
@@ -1,3 +1,23 @@
-let age = 30;
+// number & string & boolean
+let age: number = 30;
+let firstName: string = "Test";
+let isFictional: boolean;
 
-console.log(age);
+age = 20;
+firstName = "Imad";
+isFictional = false;
+
+let planet = "Earth";
+let moons = 1;
+let isLarge = false;
+
+planet = "Mars";
+moons = 145;
+isLarge = true;
+
+// null & undefined
+let something: null;
+let anthorThing: undefined;
+
+something = null;
+anthorThing = undefined;
